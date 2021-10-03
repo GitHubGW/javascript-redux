09c459a9715dc07a36c580b305b777e351aaf4fc
GW <kowonp@gmail.com>
2021-10-03 17:49:03 +0900

    Javascript counter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
+const number = document.querySelector("#number");
+const addBtn = document.querySelector("#addBtn");
+const minusBtn = document.querySelector("#minusBtn");
 
+let count = 0;
+
+number.innerText = count;
+
+const handleAddBtn = () => {
+  count = count + 1;
+  number.innerText = count;
+};
+
+const handleMinusBtn = () => {
+  count = count - 1;
+  number.innerText = count;
+};
+
+if (addBtn) {
+  addBtn.addEventListener("click", handleAddBtn);
+}
+
+if (minusBtn) {
+  minusBtn.addEventListener("click", handleMinusBtn);
+}
