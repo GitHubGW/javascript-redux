59e65bce91e13d5eafed54501ace5f3fb399774a
GW <kowonp@gmail.com>
2021-10-12 20:49:17 +0900

    Javascript & Redux

--- memo.md
+++ memo.md
@@ -121,3 +121,42 @@ store.subscribe(handleSubscribe);
 addBtn.addEventListener("click", handleAddDispatch);
 minusBtn.addEventListener("click", handleMinusDispatch);
 ```
+
+#### index.js
+
+```javascript
+import { createStore } from "redux";
+
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, { id: action.id, text: action.text }];
+    case DELETE_TODO:
+      return state;
+    default:
+      return state;
+  }
+};
+
+const handleSubscribe = () => {
+  console.log(store.getState());
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const inputValue = input.value;
+  input.value = "";
+  store.dispatch({ type: ADD_TODO, id: Date.now(), text: inputValue });
+};
+
+const store = createStore(reducer);
+store.subscribe(handleSubscribe);
+
+form.addEventListener("submit", handleSubmit);
+```
--- src/index.js
+++ src/index.js
@@ -9,27 +9,50 @@ const DELETE_TODO = "DELETE_TODO";
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [...state, { id: action.id, text: action.text }];
+      const newTodoObject = { id: action.id, text: action.text };
+      const addTodoResult = [newTodoObject, ...state];
+      return addTodoResult;
     case DELETE_TODO:
-      return state;
+      const deleteTodoResult = state.filter((state) => state.id !== action.id);
+      return deleteTodoResult;
     default:
       return state;
   }
 };
 
+const handleAddTodo = (inputValue) => {
+  store.dispatch({ type: ADD_TODO, id: Date.now(), text: inputValue });
+};
+
+const handleDeleteTodo = (event) => {
+  const parentNodeId = event.target.parentNode.id;
+  store.dispatch({ type: DELETE_TODO, id: +parentNodeId });
+};
+
 const handleSubscribe = () => {
-  console.log(store.getState());
+  const allTodos = store.getState();
+  ul.innerHTML = "";
+
+  allTodos.forEach((todo) => {
+    const li = document.createElement("li");
+    const button = document.createElement("button");
+    li.id = todo.id;
+    li.innerText = todo.text;
+    button.innerText = "Delete";
+    button.addEventListener("click", handleDeleteTodo);
+    li.appendChild(button);
+    ul.appendChild(li);
+  });
 };
 
 const handleSubmit = (event) => {
   event.preventDefault();
   const inputValue = input.value;
   input.value = "";
-  store.dispatch({ type: ADD_TODO, id: Date.now(), text: inputValue });
+  handleAddTodo(inputValue);
 };
 
 const store = createStore(reducer);
-
 store.subscribe(handleSubscribe);
 
 form.addEventListener("submit", handleSubmit);
